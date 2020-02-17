1300de70289b8bda05a618e9368e07e68b206c24
afurlaneto <afurlaneto@sorocaba.sp.gov.br>
2020-02-17 17:02:31 -0300

    seedAvatar with tematic avatars.

--- src/seeds/seedAvatars.js
+++ src/seeds/seedAvatars.js
@@ -8,16 +8,18 @@ import Avatar from '../schemas/Avatar';
 
 async function seedAvatars() {
   await Avatar.create([
-    { name: 'Avatar 1', url: 'https://avataaars.io/?avatarStyle=Circle&topType=Eyepatch&facialHairType=BeardLight&facialHairColor=Brown&clotheType=ShirtCrewNeck&clotheColor=Pink&eyeType=Side&eyebrowType=UnibrowNatural&mouthType=Tongue&skinColor=Yellow' },
-    { name: 'Avatar 2', url: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairFrizzle&accessoriesType=Kurt&hairColor=Auburn&facialHairType=BeardMagestic&facialHairColor=Blonde&clotheType=Overall&clotheColor=PastelRed&eyeType=Squint&eyebrowType=DefaultNatural&mouthType=Vomit&skinColor=DarkBrown' },
-    { name: 'Avatar 3', url: 'https://avataaars.io/?avatarStyle=Circle&topType=Hijab&accessoriesType=Kurt&hatColor=Heather&hairColor=SilverGray&facialHairType=Blank&facialHairColor=BrownDark&clotheType=Hoodie&clotheColor=PastelOrange&eyeType=Cry&eyebrowType=SadConcernedNatural&mouthType=Sad&skinColor=Black' },
-    { name: 'Avatar 4', url: 'https://avataaars.io/?avatarStyle=Circle&topType=Hat&accessoriesType=Sunglasses&hatColor=PastelOrange&facialHairType=BeardMedium&facialHairColor=Auburn&clotheType=ShirtVNeck&clotheColor=Heather&eyeType=Wink&eyebrowType=SadConcerned&mouthType=Sad&skinColor=Tanned' },
-    { name: 'Avatar 5', url: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortWaved&accessoriesType=Round&hairColor=BrownDark&facialHairType=MoustacheFancy&facialHairColor=Auburn&clotheType=BlazerShirt&clotheColor=PastelOrange&eyeType=Close&eyebrowType=AngryNatural&mouthType=Sad&skinColor=Yellow' },
-    { name: 'Avatar 6', url: 'https://avataaars.io/?avatarStyle=Circle&topType=WinterHat4&accessoriesType=Blank&hatColor=Pink&hairColor=Brown&facialHairType=MoustacheFancy&facialHairColor=Red&clotheType=ShirtScoopNeck&clotheColor=Heather&eyeType=EyeRoll&eyebrowType=Angry&mouthType=Sad&skinColor=DarkBrown' },
-    { name: 'Avatar 7', url: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairTheCaesar&accessoriesType=Round&hairColor=Brown&facialHairType=MoustacheMagnum&facialHairColor=Brown&clotheType=Hoodie&clotheColor=PastelGreen&eyeType=Dizzy&eyebrowType=Default&mouthType=Grimace&skinColor=DarkBrown' },
-    { name: 'Avatar 8', url: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairFrizzle&accessoriesType=Prescription01&hairColor=SilverGray&facialHairType=MoustacheMagnum&facialHairColor=BlondeGolden&clotheType=CollarSweater&clotheColor=Gray01&eyeType=Hearts&eyebrowType=AngryNatural&mouthType=Disbelief&skinColor=Light' },
-    { name: 'Avatar 9', url: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Blank&hairColor=Red&facialHairType=BeardMagestic&facialHairColor=Black&clotheType=BlazerSweater&clotheColor=Pink&eyeType=Side&eyebrowType=AngryNatural&mouthType=Vomit&skinColor=Pale' },
-    { name: 'Avatar 10', url: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairMiaWallace&accessoriesType=Prescription02&hairColor=Red&facialHairType=BeardMagestic&facialHairColor=Auburn&clotheType=GraphicShirt&clotheColor=PastelBlue&graphicType=Skull&eyeType=Close&eyebrowType=UpDown&mouthType=Tongue&skinColor=Tanned' },
+    { name: 'Faustão', url: 'https://cdn.glitch.com/e4d2a036-adb2-43f5-93f3-704f33cc7c0a%2Favatar_faustao.jpg?v=1581964746668' },
+    { name: 'Carminha', url: 'https://cdn.glitch.com/e4d2a036-adb2-43f5-93f3-704f33cc7c0a%2Favatar_carminha.png?v=1581964746840' },
+    { name: 'Mezenga', url: 'https://cdn.glitch.com/e4d2a036-adb2-43f5-93f3-704f33cc7c0a%2Favatar_mezenga.jpg?v=1581964746879' },
+    { name: 'Louro José', url: 'https://cdn.glitch.com/e4d2a036-adb2-43f5-93f3-704f33cc7c0a%2Favatar_lourojose.jpg?v=1581964746932' },
+    { name: 'Berdinazzi', url: 'https://cdn.glitch.com/e4d2a036-adb2-43f5-93f3-704f33cc7c0a%2Favatar_berdinazzi.jpg?v=1581964747130' },
+    { name: 'Rita', url: 'https://cdn.glitch.com/e4d2a036-adb2-43f5-93f3-704f33cc7c0a%2Favatar_rita.jpg?v=1581964747169' },
+    { name: 'Fátima', url: 'https://cdn.glitch.com/e4d2a036-adb2-43f5-93f3-704f33cc7c0a%2Favatar_f%C3%A1tima.jpg?v=1581964747210' },
+    { name: 'Ana Maria', url: 'https://cdn.glitch.com/e4d2a036-adb2-43f5-93f3-704f33cc7c0a%2Favatar_anamaria.jpg?v=1581964747483' },
+    { name: 'Pedro', url: 'https://cdn.glitch.com/e4d2a036-adb2-43f5-93f3-704f33cc7c0a%2Favatar_pedro.jpg?v=1581964747523' },
+    { name: 'Thiago Leifert', url: 'https://cdn.glitch.com/e4d2a036-adb2-43f5-93f3-704f33cc7c0a%2Favatar_thiagoleifert.jpg?v=1581964747552' },
+    { name: 'Bino', url: 'https://cdn.glitch.com/e4d2a036-adb2-43f5-93f3-704f33cc7c0a%2Favatar_bino.jpg?v=1581964747761' },
+    { name: 'Willian', url: 'https://cdn.glitch.com/e4d2a036-adb2-43f5-93f3-704f33cc7c0a%2Favatar_william.jpg?v=1581964747890' },
   ]);
   console.log('AVATARS SEEDED');
 }
